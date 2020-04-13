aba333b027214520b4e6cdb21c1693cd5abd84eb
Konstantin Knauf <knauf.konstantin@gmail.com>
2020-04-13 14:33:07 +0200

    adjust log level in source

--- src/main/java/com/ververica/platform/io/source/GithubCommitSource.java
+++ src/main/java/com/ververica/platform/io/source/GithubCommitSource.java
@@ -42,7 +42,7 @@ public class GithubCommitSource extends GithubSource<Commit> implements ListChec
   public void run(SourceContext<Commit> ctx) throws IOException {
     while (running) {
       Instant until = getUntilFor(lastTime);
-      LOG.info("Fetching commits since {} until {}", lastTime, until);
+      LOG.debug("Fetching commits since {} until {}", lastTime, until);
       PagedIterable<GHCommit> commits =
           repo.queryCommits().since(Date.from(lastTime)).until(Date.from(until)).list();
       Date lastCommitDate;
