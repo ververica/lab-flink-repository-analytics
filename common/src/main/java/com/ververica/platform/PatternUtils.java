76f4f6da66f201fda7568da5d3f08c4a1e8eb181
Nico Kruber <nico@ververica.com>
2021-05-16 22:48:30 +0200

    [common] make PatternUtils' static Pattern instances final

--- common/src/main/java/com/ververica/platform/PatternUtils.java
+++ common/src/main/java/com/ververica/platform/PatternUtils.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 /** Common store for Regexp patterns and other utility functions. */
 public class PatternUtils {
   /** Matches Flink source code components from file names (relative to the repository path). */
-  public static Pattern SOURCE_FILENAME_COMPONENT_PATTERN =
+  public static final Pattern SOURCE_FILENAME_COMPONENT_PATTERN =
       Pattern.compile(
           "^(?<component>.+?(?=/src/.*|pom.xml|README.md)|(?:flink-)?docs(?=/.*)|tools(?=/.*)|flink-python(?=/.*)|flink-end-to-end-tests/test-scripts(?=/.*)|flink-scala-shell(?=/start-script/.*)|flink-container(?=/.*)|flink-contrib/docker-flink(?=/.*)|flink-table/flink-sql-client(?=/.*)|flink-end-to-end-tests(?=/[^/]*\\.sh)).*?");
 
@@ -13,23 +13,23 @@ public class PatternUtils {
    * Normalises email subjects to a common representation, removing things that email editors add
    * when replying.
    */
-  public static Pattern EMAIL_SUBJECT_THREAD_PATTERN =
+  public static final Pattern EMAIL_SUBJECT_THREAD_PATTERN =
       Pattern.compile("^\\s*(?:(?:Re|AW):\\s*)*(?<emailthread>.*?)\\s*$", Pattern.CASE_INSENSITIVE);
 
   /** Finds a Jira ticket action and number from the message's "Subject" field (as sent by Jira). */
-  public static Pattern EMAIL_SUBJECT_JIRA_TICKET_PATTERN =
+  public static final Pattern EMAIL_SUBJECT_JIRA_TICKET_PATTERN =
       Pattern.compile(
           "\\[jira\\]\\s*\\[(?<ticketaction>.*)\\]\\s*\\((?<ticketnumber>FLINK-[0-9]+)\\).*");
 
   /** Finds a Jira ticket author from the message's "From" field (as sent by Jira). */
-  public static Pattern EMAIL_FROM_JIRA_TICKET_AUTHOR_PATTERN =
+  public static final Pattern EMAIL_FROM_JIRA_TICKET_AUTHOR_PATTERN =
       Pattern.compile("\"(?<ticketauthor>.*)\\s*\\((?:Jira|JIRA)\\)\"\\s*<jira@apache.org>");
 
   /**
    * Matches a line of the email's text body that contains component information (using the message
    * format sent by Jira).
    */
-  public static Pattern EMAIL_BODY_JIRA_TICKET_COMPONENTS_PATTERN =
+  public static final Pattern EMAIL_BODY_JIRA_TICKET_COMPONENTS_PATTERN =
       Pattern.compile(" {10}Components: (?<components>.*)");
 
   /**
@@ -40,6 +40,6 @@ public class PatternUtils {
    * <p>It uses a positive look-ahead to search for all ", " followed by any pairwise bracketed
    * content (if available).
    */
-  public static Pattern EMAIL_BODY_JIRA_TICKET_COMPONENTS_SPLIT_PATTERN =
+  public static final Pattern EMAIL_BODY_JIRA_TICKET_COMPONENTS_SPLIT_PATTERN =
       Pattern.compile(", (?=(?:[^(]*\\([^)]*\\))*[^)]*$)");
 }
